338a6bad4c089386c6d20d62f8e677ddca4ff03c
Tdonnelly20 <tate.donne@gmail.com>
2022-11-12 15:02:33 -0500

    Documentation

--- RomaniaCityApp.py
+++ RomaniaCityApp.py
@@ -5,7 +5,7 @@ from Problem import *
 
 def main():
     answer = input('Should I read the romania map (Yes/No): ')
-    if answer == 'Yes':
+    if answer.lower() == 'yes':
         map = create_map('romania_map')
         map.locations = dict(
         Arad=(91, 492), Bucharest=(400, 327), Craiova=(253, 288),
@@ -15,12 +15,9 @@ def main():
         Oradea=(131, 571), Pitesti=(320, 368), Rimnicu=(233, 410),
         Sibiu=(207, 457), Timisoara=(94, 410), Urziceni=(456, 350),
         Vaslui=(509, 444), Zerind=(108, 531))
-        print(map.nodes())
         while (True):
             city1 = input('Please enter city 1: ')
             city2 = input('Please enter city 2: ')
-            #city1 = 'Sibiu'
-            #city2 = 'Zerind'
             if city1 == city2:
                 print("The cities you entered are the same. Please enter different cities.")
             elif not map.nodes().__contains__(city1):
@@ -28,6 +25,7 @@ def main():
             elif not map.nodes().__contains__(city2):
                 print(f"{city2} is not on the map. Please enter valid cities")
             else:
+                '''If the cities inputted are valid find the cost between cities and the path between them'''
                 find_path(city1, city2, map)
                 answer = input(f'Would you like to find the optimal path between any 2 cities again? (Yes/No): ')
                 if answer.lower() != 'yes':
@@ -36,15 +34,13 @@ def main():
     else:
         print("Thank You for Using Our App")
 
-
+'''Finds the optimal path between 2 cities in map using the A* search algorithm'''
 def find_path(city1, city2, map):
     agent = SimpleProblemSolvingAgent.SimpleProblemSolvingAgentProgram(city1)
     problem = GraphProblem(city1, city2, map)
 
     path = agent.search(problem)
     pathList = path.path()
-    #cost=problem.path_cost()
-    path.path_cost
 
     print("A* Search")
     print(f"\t Cost: {path.path_cost}")
@@ -55,6 +51,7 @@ def find_path(city1, city2, map):
     print()
 
 
+'''Reads values in file and translates that to map links'''
 def create_map(filepath):
     graph = search.Graph()
     file = open(filepath)
@@ -62,8 +59,6 @@ def create_map(filepath):
         vals = i.split()
         graph.connect1(vals[0], vals[1], int(vals[2]))
         graph.connect1(vals[1], vals[0], int(vals[2]))
-
-    #print(graph.graph_dict)
     return graph
 
 
