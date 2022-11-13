eb6b43e20ebdfdd21334215df94cabd717fd8b16
AlexMarrinan <alex@balroe.com>
2022-11-12 20:19:32 -0500

    json romania map

--- RomaniaCityApp.py
+++ RomaniaCityApp.py
@@ -1,24 +1,14 @@
 import SimpleProblemSolvingAgent
 import search
+import json
 from Problem import *
 
 
 def main():
-    answer = input('What map file should we read?: ')
-    if answer != '':
-        map = create_map(answer)
-        while map == None:
-            answer = input('What map file should we read?: ')
-            map = create_map(answer)
+    answer = input('Would you like to read the romainia map? (Yes/No):  ')
+    if answer == 'Yes':
+        map = create_map("romania_map.json")
         print(f"Loading from {answer} ...")
-        map.locations = dict(
-        Arad=(91, 492), Bucharest=(400, 327), Craiova=(253, 288),
-        Drobeta=(165, 299), Eforie=(562, 293), Fagaras=(305, 449),
-        Giurgiu=(375, 270), Hirsova=(534, 350), Iasi=(473, 506),
-        Lugoj=(165, 379), Mehadia=(168, 339), Neamt=(406, 537),
-        Oradea=(131, 571), Pitesti=(320, 368), Rimnicu=(233, 410),
-        Sibiu=(207, 457), Timisoara=(94, 410), Urziceni=(456, 350),
-        Vaslui=(509, 444), Zerind=(108, 531))
         while (True):
             city1 = input('Please enter city 1: ')
             city2 = input('Please enter city 2: ')
@@ -33,10 +23,10 @@ def main():
                 find_path(city1, city2, map)
                 answer = input(f'Would you like to find the optimal path between any 2 cities again? (Yes/No): ')
                 if answer.lower() != 'yes':
-                    print("Thank You for Using Our App")
+                    print("Thank You for Using Our App!")
                     break
     else:
-        print("Thank You for Using Our App")
+        print("Thank You for Using Our App!")
 
 '''Finds the optimal path between 2 cities in map using the A* search algorithm'''
 def find_path(city1, city2, map):
@@ -57,16 +47,19 @@ def find_path(city1, city2, map):
 
 '''Reads values in file and translates that to map links'''
 def create_map(filepath):
-    graph = search.Graph()
     try:
         file = open(filepath)
     except:
         print(f"File {filepath} not found!")
         return None
-    for i in file.readlines():
-        vals = i.split()
-        graph.connect1(vals[0], vals[1], int(vals[2]))
-        graph.connect1(vals[1], vals[0], int(vals[2]))
+    data = json.load(file)
+
+    graphData = data["graph"]
+    locationsData = data["locations"]
+
+    graph = search.Graph(graphData, False)
+    graph.locations = locationsData
+
     return graph
 
 
