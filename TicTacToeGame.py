b0b66492bb0766e92300639e00a02ed72c1d0174
Alex Marrinan <57539939+AlexMarrinan@users.noreply.github.com>
2022-11-22 14:32:08 -0500

    more basic ui

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -9,29 +9,23 @@ def main():
         v = input("Please insert v: ")
         k = input("Please insert k: ")
         print('AI playing with Minmax Decision')
+        searchType = 1
         game = TicTacToeClass(int(h), int(v), int(k))
-        state = game.initial
-        while (not game.terminal_test(state)):
-            move = minmax_decision(state, game)
-            print(move)
-            state = game.result(state, move)
-            print(game.display(state))
+        while True:
+            state = game.initial
+            while (not game.terminal_test(state)):
+                if (searchType == 0):
+                    move = minmax_decision(state, game)
+                else:
+                    move = alpha_beta_search(state, game)
+                print(move)
+                state = game.result(state, move)
+                print(game.display(state))
 
             answer = input("Would you like to run TicTacToe again? ")
             if (answer.lower() != 'yes'):
                 print('Thank You for Playing Our Game')
                 break
-        
-        
-        
-        print('Alpha-Beta Pruning Search')
-'''        while (True):
-            game = TicTacToeClass(int(h), int(v), int(k))
-            print(game.h)
-            answer = input("Would you like to run TicTacToe again? ")
-            if (answer.lower() != 'yes'):
-                print('Thank You for Playing Our Game')
-                break'''
 
 
 def mainAgain():
