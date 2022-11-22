3872769eb3acf45601b9f2c684de53eaf402f3b7
spateyz <94015210+spateyz@users.noreply.github.com>
2022-11-22 14:40:04 -0500

    user input but its brocken

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -8,19 +8,29 @@ def main():
         h = input("Please insert h: ")
         v = input("Please insert v: ")
         k = input("Please insert k: ")
+        turn = 0
+        first = input("Do you want to go first? (0 - yes, 1 - no)")
+        if first != 0:
+            turn = 1
         print('AI playing with Minmax Decision')
         searchType = 1
         game = TicTacToeClass(int(h), int(v), int(k))
         while True:
             state = game.initial
-            while (not game.terminal_test(state)):
-                if (searchType == 0):
-                    move = minmax_decision(state, game)
+            while not game.terminal_test(state):
+                if(turn %2 == 0):
+                    if (searchType == 0):
+                        move = minmax_decision(state, game)
+                    else:
+                        move = alpha_beta_search(state, game)
                 else:
-                    move = alpha_beta_search(state, game)
+                    x = input("Please enter x coordinate of your move: ")
+                    y = input("Please enter x coordinate of your move: ")
+                    move = x,y
                 print(move)
                 state = game.result(state, move)
                 print(game.display(state))
+                move += 1
 
             answer = input("Would you like to run TicTacToe again? ")
             if (answer.lower() != 'yes'):
