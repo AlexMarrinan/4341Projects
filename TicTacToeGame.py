907ca9f6a2f0307e657b4ee8866af017ec2ee688
Tdonnelly20 <tate.donne@gmail.com>
2022-11-22 14:36:56 -0500

    Input for algs

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -8,15 +8,16 @@ def main():
         h = input("Please insert h: ")
         v = input("Please insert v: ")
         k = input("Please insert k: ")
-        print('AI playing with Minmax Decision')
-        searchType = 1
         game = TicTacToeClass(int(h), int(v), int(k))
         while True:
             state = game.initial
             while (not game.terminal_test(state)):
-                if (searchType == 0):
+                searchAlg=input("What search alg should we use? Minmax or AlphaBeta?: ")
+                if searchAlg.lower() == 'minmax':
+                    print('AI playing with Minmax Decision')
                     move = minmax_decision(state, game)
                 else:
+                    print('AI playing with Alpha-Beta Search')
                     move = alpha_beta_search(state, game)
                 print(move)
                 state = game.result(state, move)
