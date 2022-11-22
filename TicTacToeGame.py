0bbcc95d5b809f24dc249109b57c176429e0d133
Alex Marrinan <57539939+AlexMarrinan@users.noreply.github.com>
2022-11-22 14:28:00 -0500

    we made ai!!!11!!!!11

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,4 +1,5 @@
 from TicTacToeClass import TicTacToeClass
+from games4e import *
 
 
 def main():
@@ -8,25 +9,40 @@ def main():
         v = input("Please insert v: ")
         k = input("Please insert k: ")
         print('AI playing with Minmax Decision')
-        while (True):
-            game = TicTacToeClass(int(h), int(v), int(k))
-            print(game.h)
+        game = TicTacToeClass(int(h), int(v), int(k))
+        state = game.initial
+        while (not game.terminal_test(state)):
+            move = minmax_decision(state, game)
+            print(move)
+            state = game.result(state, move)
+            print(game.display(state))
+
             answer = input("Would you like to run TicTacToe again? ")
             if (answer.lower() != 'yes'):
                 print('Thank You for Playing Our Game')
                 break
+        
+        
+        
         print('Alpha-Beta Pruning Search')
-        while (True):
+'''        while (True):
             game = TicTacToeClass(int(h), int(v), int(k))
             print(game.h)
             answer = input("Would you like to run TicTacToe again? ")
             if (answer.lower() != 'yes'):
                 print('Thank You for Playing Our Game')
-                break
+                break'''
 
-def results(self):
 
-    # print(f'Minmax {type(minmax_decision(state, self.game))}')
+def mainAgain():
+    game = TicTacToeClass(3, 3, 3)
+    state = game.initial
+    print(game.display(state))
 
-if __name__ == "__main__":
+    move = minmax_decision(game.initial, game)
+    print(move)
+    state = game.result(state, move)
+    print(game.display(state))
+
+if __name__=="__main__":
     main()
