313b7265bb2f23e8f3b27a5119bc6cec6e0d7113
spateyz <94015210+spateyz@users.noreply.github.com>
2022-11-22 14:14:26 -0500

    aaaaaaaaaaaaaghhh

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -7,6 +7,15 @@ def main():
         h = input("Please insert h: ")
         v = input("Please insert v: ")
         k = input("Please insert k: ")
+        print('AI playing with Minmax Decision')
+        while (True):
+            game = TicTacToeClass(int(h), int(v), int(k))
+            print(game.h)
+            answer = input("Would you like to run TicTacToe again? ")
+            if (answer.lower() != 'yes'):
+                print('Thank You for Playing Our Game')
+                break
+        print('Alpha-Beta Pruning Search')
         while (True):
             game = TicTacToeClass(int(h), int(v), int(k))
             print(game.h)
@@ -15,10 +24,8 @@ def main():
                 print('Thank You for Playing Our Game')
                 break
 
-
 def results(self):
-    print('Minmax Decision')
-    print('Alpha-Beta Pruning Search')
+
     # print(f'Minmax {type(minmax_decision(state, self.game))}')
 
 if __name__ == "__main__":
